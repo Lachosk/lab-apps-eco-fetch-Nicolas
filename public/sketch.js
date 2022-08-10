f7ac774f76624012036a1f712dc8b01ee7948c84
Lachosk <1006009500@u.icesi.edu.co>
2022-08-10 18:50:32 -0500

    Corrige errores y posicion de los elemtos

--- public/sketch.js
+++ public/sketch.js
@@ -8,6 +8,7 @@ let URL5 = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population'
 
 
 let img;
+let img2;
 
 let catFact = null;
 let dogImg = null;
@@ -33,7 +34,6 @@ function setup() {
         .then(response => response.json())
         .then(data => {
             dogImg = data
-            console.log(data)
             img = loadImage(dogImg.message);
         });
 
@@ -47,6 +47,7 @@ function setup() {
         .then(response => response.json())
         .then(data => {
             dataUser = data
+            //img2 = loadImage(data.results[0].picture.large)
         });
 
     fetch(URL5)
@@ -61,15 +62,12 @@ function setup() {
             .then(response => response.json())
             .then(data => {
                 catFact = data
-                console.log(data)
-                console.log(catFact.fact)
             })
 
         fetch(URL2)
             .then(response => response.json())
             .then(data => {
                 dogImg = data
-                console.log(data)
                 img = loadImage(dogImg.message);
             });
 
@@ -77,63 +75,68 @@ function setup() {
             .then(response => response.json())
             .then(data => {
                 bitcoin = data
-                console.log(data)
-                console.log(bitcoin.time.updated);
-                console.log(bitcoin.chartName)
-                console.log(bitcoin.bpi.USD.code)
-                console.log(bitcoin.bpi.USD.rate)
             });
 
         fetch(URL4)
             .then(response => response.json())
             .then(data => {
                 dataUser = data
-                console.log(data)
-                console.log(dataUser.results[0].name.first)
-                console.log(dataUser.results[0].name.last)
+                //img2 = loadImage(data.results[0].picture.large)
             });
 
         fetch(URL5)
             .then(response => response.json())
             .then(data => {
                 dataNation = data
-                console.log(dataNation.source[0].annotations.source_name)
             });
     }
 }
 
 function draw() {
-    background(0);
+    background(100);
     newCursor();
 
     fill(255);
     textSize(20);
     textWrap(WORD);
-    text("Click para cambiar", 500, 500, 300)
+    text("Click para cambiar", windowWidth/4, windowHeight/4, 300)
 
     if (catFact != null) {
+        fill(255);
+        text("CatFact", windowWidth / 12, windowHeight / 11, 300)
         fill(255, 255, 0)
-        text(catFact.fact, 100, 100, 300)
+        text(catFact.fact, windowWidth / 12, windowHeight / 8, 300)
     }
 
     if (dogImg != null) {
-        image(img, windowWidth / 2, 0, 300, 300);
+        fill(255);
+        text("Dog Random IMG", windowWidth / 2, windowHeight / 11, 300)
+        image(img, windowWidth / 2, windowHeight / 8, 300, 300);
     }
 
     if (bitcoin != null) {
+        fill(255)
+        text(bitcoin.chartName, windowWidth / 12, windowHeight - 480, 300)
         fill(0, 255, 0)
-        text(bitcoin.chartName, 100, 700, 300)
-        text("Fecha: " + bitcoin.time.updated, 100, 400, 300)
-        text("Moneda: " + bitcoin.bpi.USD.code, 100, 450, 300)
-        text("Precio: " + bitcoin.bpi.USD.rate, 100, 500, 300)
+        text("Fecha: " + bitcoin.time.updated, windowWidth / 12, windowHeight - 450, 300)
+        text("Moneda: " + bitcoin.bpi.USD.code, windowWidth / 12, windowHeight - 400, 300)
+        text("Precio: " + bitcoin.bpi.USD.rate, windowWidth / 12, windowHeight - 375, 300)
     }
     if (dataUser != null) {
+        fill(255);
+        text("Random User", windowWidth / 2, windowHeight - 480,300)
         fill(0, 255, 255)
-        text(dataUser.results[0].name.first + " " + dataUser.results[0].name.last, 700, 400, 300)
+        text("Nombre: " + dataUser.results[0].name.first + " " + dataUser.results[0].name.last, windowWidth / 2, windowHeight - 450, 300)
+        text("Genero: " + dataUser.results[0].gender, windowWidth / 2, windowHeight - 420, 300)
+        text("Edad: " + dataUser.results[0].registered.age, windowWidth / 2, windowHeight - 390, 300)
+       //no pinta la imagen y tira error porque dice que no tengo acceso idk no se como arreglar esto
+        //image(img2,windowWidth / 2, windowHeight - 420,100,100)
     }
     if (dataNation != null) {
-        fill(255, 0, 255)
-        text(dataNation.source[0].annotations.source_name, 700, 600, 300)
+        fill(255);
+        text("Data Nation",windowWidth/12, windowHeight-230,300)
+        fill(100, 0, 255)
+        text(dataNation.source[0].annotations.source_name, windowWidth/12, windowHeight-200, 300)
     }
 
 }
